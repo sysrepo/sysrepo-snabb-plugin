45f5c23a3365a1393b9998f51e0ca1c43a11ea80
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-09-20 18:46:43 +0200

    use YANG model enabled feature from Sysrepo
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- src/parse.c
+++ src/parse.c
@@ -37,7 +37,10 @@ parse_yang_model(ctx_t *ctx) {
 	const struct lys_module *module = NULL;
     struct ly_ctx *libyang_ctx = NULL;
 	char *schema_content = NULL;
+	sr_schema_t *schemas = NULL;
+	size_t schema_cnt = 0;
 	int rc = SR_ERR_OK;
+	int lrc = 0;
 
 	/* TODO get sysrepo yang schema path
 	 * will be neede for alarms
@@ -57,11 +60,28 @@ parse_yang_model(ctx_t *ctx) {
 		goto error;
 	}
 
+	/* fetch enbaled features from Sysrepo and enable them in libyang */
+	rc = sr_list_schemas(ctx->sess, &schemas, &schema_cnt);
+	for (size_t s = 0; s < schema_cnt; s++) {
+		if (0 == strcmp(ctx->yang_model, schemas[s].module_name)){
+			for (size_t i = 0; i < schemas[s].enabled_feature_cnt; i++) {
+				INF("Enable feature %s in yang model %s", schemas[s].enabled_features[i], ctx->yang_model);
+				lrc = lys_features_enable(module, schemas[s].enabled_features[i]);
+				if (0 != lrc) {
+					ERR("The feature %s is not defined in the yang model %s", schemas[s].enabled_features[i], ctx->yang_model);
+				}
+			}
+		}
+	}
+
 	ctx->module = module;
 
 	ctx->libyang_ctx = libyang_ctx;
 
 error:
+	if (NULL != schemas && schema_cnt > 0) {
+		sr_free_schemas(schemas, schema_cnt);
+	}
 	if (NULL != schema_content) {
 		free(schema_content);
 	}
