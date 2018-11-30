b674e668b71c0c42084383163b86965ddfd96beb
Mislav Novakovic <mislav.novakovic@sartura.hr>
2018-11-30 18:54:11 +0100

    update logic for SYSREPO_LESS_0.7.7
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- src/snabb.c
+++ src/snabb.c
@@ -156,12 +156,10 @@ error:
 static int
 #ifdef SYSREPO_LESS_0_7_5
 state_data_cb(const char *xpath, sr_val_t **values, size_t *values_cnt, void *private_ctx)
-#else
-#ifdef SYSREPO_LESS_0_7_7
+#elif SYSREPO_LESS_0_7_7
 state_data_cb(const char *xpath, sr_val_t **values, size_t *values_cnt, uint64_t request_id, void *private_ctx)
 #else
 state_data_cb(const char *xpath, sr_val_t **values, size_t *values_cnt, uint64_t request_id, const char *original_xpath, void *private_ctx)
-#endif
 #endif
 {
 	int rc = SR_ERR_OK;
